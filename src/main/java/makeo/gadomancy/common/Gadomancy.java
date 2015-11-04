f43012a3279948bd6d73c9118d60cb070e346ca9
makeo <mak321@web.de>
2015-11-04 21:59:39 +0100

    changed version and added hell to author list

--- src/main/java/makeo/gadomancy/common/Gadomancy.java
+++ src/main/java/makeo/gadomancy/common/Gadomancy.java
@@ -25,7 +25,7 @@ public class Gadomancy
     public static final String MODID = "gadomancy";
     public static final String NAME = "Gadomancy";
 
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     private static final String PROXY_CLIENT = "makeo.gadomancy.client.ClientProxy";
     private static final String PROXY_SERVER = "makeo.gadomancy.common.CommonProxy";
