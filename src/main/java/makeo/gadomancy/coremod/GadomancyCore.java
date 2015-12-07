ae0367a5bde1631f1e0c4c373f6c908f43b0defd
makeo <mak321@web.de>
2015-12-07 21:50:29 +0100

    -"added core mod"

---
+++ src/main/java/makeo/gadomancy/coremod/GadomancyCore.java
@@ -0,0 +1,51 @@
+package makeo.gadomancy.coremod;
+
+import cpw.mods.fml.relauncher.IFMLCallHook;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+
+import java.util.Map;
+
+/**
+ * This class is part of the Gadomancy Mod
+ * Gadomancy is Open Source and distributed under the
+ * GNU LESSER GENERAL PUBLIC LICENSE
+ * for more read the LICENSE file
+ * <p/>
+ * Created by makeo @ 07.12.2015 21:17
+ */
+@IFMLLoadingPlugin.TransformerExclusions({"makeo.gadomancy.api", "makeo.gadomancy.client", "makeo.gadomancy.common"})
+public class GadomancyCore implements IFMLLoadingPlugin, IFMLCallHook {
+    public GadomancyCore() {
+        System.out.println("ITSSSS AA LIIEEEEEE");
+    }
+
+    @Override
+    public Void call() throws Exception {
+        return null;
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return null;
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
---
+++ src/main/java/makeo/gadomancy/coremod/GadomancyTransformer.java
@@ -0,0 +1,20 @@
+package makeo.gadomancy.coremod;
+
+import cpw.mods.fml.common.asm.transformers.AccessTransformer;
+
+import java.io.IOException;
+
+/**
+ * This class is part of the Gadomancy Mod
+ * Gadomancy is Open Source and distributed under the
+ * GNU LESSER GENERAL PUBLIC LICENSE
+ * for more read the LICENSE file
+ * <p/>
+ * Created by makeo @ 07.12.2015 21:48
+ */
+public class GadomancyTransformer extends AccessTransformer {
+    public GadomancyTransformer() throws IOException {
+    }
+
+
+}
